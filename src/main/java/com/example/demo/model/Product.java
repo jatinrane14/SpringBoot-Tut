2292a11d5bc377a9ad70164dc890a365cc714995
jatinrane14 <jatinrane23@gmail.com>
2026-03-27 19:59:16 +0530

    Connect Application to MYSQL Database

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,9 +1,20 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
 public class Product {
+    @Id
     private int prodId;
     private String prodName;
     private int price;
+
+    public Product() {
+
+    }
 
     public int getPrice() {
         return price;
@@ -24,6 +35,7 @@ public class Product {
     public String getProdName() {
         return prodName;
     }
+
 
     public Product(int price, int prodId, String prodName) {
         this.price = price;
--- src/main/java/com/example/demo/services/ProductService.java
+++ src/main/java/com/example/demo/services/ProductService.java
@@ -1,48 +1,58 @@
 package com.example.demo.services;
 
 import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 @Service
 public class ProductService {
-    List<Product> product = new ArrayList<>(Arrays.asList(new Product(50000,100,"Laptop")));
+    @Autowired
+    ProductRepo prRepo;
+
+//    List<Product> product = new ArrayList<>(Arrays.asList(new Product(50000,100,"Laptop")));
     public List<Product> getProducts(){
-        return product;
+        return prRepo.findAll();
     }
 
     public List<Product> getProduct(int prodId){
-        return product.stream().filter(f -> f.getProdId() ==prodId).toList();
+//        return product.stream().filter(f -> f.getProdId() ==prodId).toList();
+        return Collections.singletonList(prRepo.findById(prodId).orElse(new Product()));
     }
 
     public String addProduct(Product prodData){
-        product.add(prodData);
+//        product.add(prodData);
+        prRepo.save(prodData);
         return "Data Inserted successfully!";
     }
 
     public void updateProduct(Product prod) {
-        System.out.println(prod);
-        int index =0;
-        for(int i=0;i<product.size();i++){
-            if(product.get(i).getProdId() == prod.getProdId()){
-                index = i;
-            }
-            System.out.println(i);
-        }
-        product.set(index,prod);
+//        System.out.println(prod);
+//        int index =0;
+//        for(int i=0;i<product.size();i++){
+//            if(product.get(i).getProdId() == prod.getProdId()){
+//                index = i;
+//            }
+//            System.out.println(i);
+//        }
+//        product.set(index,prod);
+        prRepo.save(prod);
     }
 
     public void deleteProduct(Product prod) {
-        int index =0;
-        for(int i=0;i<product.size();i++){
-            if(product.get(i).getProdId() == prod.getProdId()){
-                index = i;
-            }
-            System.out.println(i);
-        }
-        product.remove(index);
+//        int index =0;
+//        for(int i=0;i<product.size();i++){
+//            if(product.get(i).getProdId() == prod.getProdId()){
+//                index = i;
+//            }
+//            System.out.println(i);
+//        }
+//        product.remove(index);
+        prRepo.delete(prod);
     }
 }
